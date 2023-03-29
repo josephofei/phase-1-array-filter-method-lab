6057746777ad4ae53e8e8d50c60ee2d8bada6a3c
Joseph Ofei <josephofei24@gmail.com>
2023-03-29 11:54:20 -0400

    complete array filter

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution here
+
+
+
+/*function findMatching(collection, string) {
+    for (const user of collection) {
+        if (string === user) {
+            let result = drivers.Filter(findMatching(drivers, string))
+            return [result]
+        }
+
+
+    }
+}*/
+
+
+function findMatching(drivers, attribute) {
+    return drivers.filter(function (driver) { return driver.toLowerCase() === attribute.toLowerCase() })
+}
+
+function fuzzyMatch(drivers, attribute) {
+    return drivers.filter(function (driver) { return driver.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase() })
+}
+
+function matchName(drivers, argument) {
+    return drivers.filter(function (driver) { return driver.name === argument })
+}
+
+
+
+
